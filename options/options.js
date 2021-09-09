f74e6170afb2044cf61bf1331bb411141882c50f
Crowdbotics <team@crowdbotics.com>
2021-09-09 13:02:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tst_daniel_figma_im_22901Identifier",
+  url: "https://tst_daniel_figma_im_22901Identifier.botics.co",
+  api: "https://tst_daniel_figma_im_22901Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
